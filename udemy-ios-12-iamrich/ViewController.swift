e3991146b725e17d2694d6c15def409b13ad5ec8
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2019-06-18 16:41:50 -0300

    Initial Commit

---
+++ udemy-ios-12-iamrich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  udemy-ios-12-iamrich
+//
+//  Created by Eduardo Ribeiro da Silva on 18/06/19.
+//  Copyright © 2019 Eduardo Ribeiro da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
